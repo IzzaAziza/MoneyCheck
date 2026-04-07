8b2bba5931edbd0b12eb02d658a5a5b2551fd9ff
Baiq Izza Aziza <bqizza1736@gmail.com>
2026-04-07 18:42:15 +0700

    Menambahkan Gambar

--- app/src/main/java/com/baiqizzaaziza0105/moneycheck/MainActivity.kt
+++ app/src/main/java/com/baiqizzaaziza0105/moneycheck/MainActivity.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -13,9 +19,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.baiqizzaaziza0105.moneycheck.ui.theme.MoneyCheckTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,11 +60,26 @@ fun MainScreen() {
 }
 
 @Composable
-fun ScreenContent (modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android!",
+fun ScreenContent(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.money),
+            contentDescription = "Money Image",
+            modifier = Modifier.size(150.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Yuk cek kondisi keuanganmu Bulan ini!"
+        )
+    }
 }
 
 @Preview(showBackground = true)
