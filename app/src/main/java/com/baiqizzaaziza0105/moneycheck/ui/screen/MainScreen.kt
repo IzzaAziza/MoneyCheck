2b11161aa154c3e3fdfad18c8300ef664ada6dea
Baiq Izza Aziza <bqizza1736@gmail.com>
2026-04-07 19:44:13 +0700

    Meminta input dari pengguna + checkbox

--- app/src/main/java/com/baiqizzaaziza0105/moneycheck/ui/screen/MainScreen.kt
+++ app/src/main/java/com/baiqizzaaziza0105/moneycheck/ui/screen/MainScreen.kt
@@ -2,23 +2,37 @@ package com.baiqizzaaziza0105.moneycheck.ui.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.baiqizzaaziza0105.moneycheck.R
@@ -46,10 +60,18 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
+    var pemasukan by remember { mutableStateOf("") }
+    var pengeluaran by remember { mutableStateOf("") }
+
+    var punyaTabungan by remember { mutableStateOf(false) }
+
+    var jumlahTabungan by remember { mutableStateOf("") }
+
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
@@ -62,8 +84,59 @@ fun ScreenContent(modifier: Modifier = Modifier) {
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(
-            text = "Yuk cek kondisi keuanganmu Bulan ini!"
+            text = stringResource(id = R.string.MC_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
         )
+        OutlinedTextField(
+            value = pemasukan,
+            onValueChange = { pemasukan = it},
+            label = { Text(text = stringResource(R.string.pemasukan)) },
+            leadingIcon = { Text(text = "Rp")},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = pengeluaran,
+            onValueChange = {pengeluaran = it},
+            label = {Text(text = stringResource(id = R.string.pengeluaran)) },
+            leadingIcon = { Text(text = "Rp") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Checkbox(
+                checked = punyaTabungan,
+                onCheckedChange = { punyaTabungan = it },
+            )
+            Text(text = stringResource(id = R.string.tabungan))
+        }
+        if (punyaTabungan) {
+            OutlinedTextField(
+                value = jumlahTabungan,
+                onValueChange = { jumlahTabungan = it },
+                label = { Text( text =stringResource(R.string.jumlah_tabungan)) },
+                leadingIcon = { Text("Rp") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
     }
 }
 
